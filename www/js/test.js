336df3a47db04b8f3ce8d89a162dedce0c8097b8
George Dan Foia <foia.dan@gmail.com>
2016-03-16 13:25:11 +0000

    small update

--- www/js/test.js
+++ www/js/test.js
@@ -1,18 +1,11 @@
 
-// var nav = document.getElementsByClassName("nav")[0];
-// nav.addEventListener("click", addClass(), flase);
+
 
 var el = document.getElementsByClassName("nav")[0];
 
-var addClass = function(element){
-	element.className += " isActive"; 
+var toggleClass = function(element){
+	element.classList.toggle("isActive"); 
 }
 
-el.addEventListener("touchstart", function() { addClass(el); }, false);
+el.addEventListener("touchstart", function() { toggleClass(el); }, false);
 
-
-
-// var addClass = function(){
-//     alert("test");
-// }
-// console.log(nav);
